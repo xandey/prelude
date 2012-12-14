fb93551c2d13638503dc279c430ac01bc2c288cd
Alexander Patterson IV <xandey@gmail.com>
2012-12-14 10:22:08 -0500

    larger window size

--- personal/init.el
+++ personal/init.el
@@ -2,6 +2,27 @@
 (add-to-list 'package-archives
              '("marmalade" . "http://marmalade-repo.org/packages/"))
 
+;; setup window size according to resolution
+(defun set-frame-size-according-to-resolution ()
+  (interactive)
+  (if window-system
+      (progn
+        ;; use 120 char wide window for largeish displays
+        ;; and smaller 80 column windows for smaller displays
+        ;; pick whatever numbers make sense for you
+        (if (> (x-display-pixel-width) 1280)
+            (add-to-list 'default-frame-alist (cons 'width 180))
+          (add-to-list 'default-frame-alist (cons 'width 120)))
+        ;; for the height, subtract a couple hundred pixels
+        ;; from the screen height (for panels, menubars and
+        ;; whatnot), then divide by the height of a char to
+        ;; get the height we want
+        (add-to-list 'default-frame-alist 
+                     (cons 'height (/ (- (x-display-pixel-height) 100)
+                                      (frame-char-height)))))))
+
+(set-frame-size-according-to-resolution)
+
 ;; indentation style, i like this better:
 (setq c-default-style "linux" c-basic-offset 2)
 
@@ -36,6 +57,7 @@
 (if (string-equal "darwin" (symbol-name system-type))
     (setq tramp-persistency-file-name "~/.emacs.d/tramp-darwin")
   (setq tramp-persistency-file-name "~/.emacs.d/tramp-linux"))
+
 
 ;; ;;prelude stuff:
 ;; ;; remove guru mode and allow arrows:
